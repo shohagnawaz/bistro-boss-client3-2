b9904c18e98658732115a34381834214dc139c2f
shohagnawaz <snawazdigital@gmail.com>
2025-01-06 23:40:37 +0600

    form

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,4 +1,12 @@
+import { useForm } from "react-hook-form";
+
 const SignUp = () => {
+  
+  const { register, handleSubmit, formState: { errors }, } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -11,12 +19,28 @@ const SignUp = () => {
           </p>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                {...register("name", { required: true})}
+                name="name"
+                type="text"
+                placeholder="name"
+                className="input input-bordered"
+                required
+                // {errors.name && <span>This field is required</span>}
+              />
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
+                {...register("email")}
+                name="email"
                 type="email"
                 placeholder="email"
                 className="input input-bordered"
@@ -28,19 +52,16 @@ const SignUp = () => {
                 <span className="label-text">Password</span>
               </label>
               <input
+                {...register("password")}
+                name="password"
                 type="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
               />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <button className="btn btn-primary">Sign up</button>
             </div>
           </form>
         </div>
