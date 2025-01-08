579c81b57b1734b086a37ecaca0e10e0d1210b1f
shohagnawaz <snawazdigital@gmail.com>
2025-01-08 15:25:37 +0600

    ax

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,14 +1,15 @@
 import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
-import axios from "axios";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const FoodCard = ({item}) => {
     const { name, image, price, recipe, _id } = item;
     const { user } = useAuth();
     const navigate = useNavigate();
     const location = useLocation();
-    
+    const axiosSecure = useAxiosSecure();
+
     const handleAddToCart = (food) => {
       if(user && user.email) {
         // send cart item to the database
@@ -20,7 +21,7 @@ const FoodCard = ({item}) => {
           image, 
           price
         }
-        axios.post('http://localhost:5000/carts', cartItem)
+        axiosSecure.post('/carts', cartItem)
         .then(res => {
           console.log(res.data);
           if(res.data.insertedId) {
