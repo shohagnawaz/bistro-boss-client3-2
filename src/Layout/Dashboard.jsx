a23cc4dbeec2b890fb11c4b1fcd7b15751c93655
shohagnawaz <snawazdigital@gmail.com>
2025-01-09 18:46:49 +0600

    cart

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,12 +1,14 @@
-import { FaAd, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
+import { FaAd, FaHome, FaList, FaSearch, FaShoppingCart } from "react-icons/fa";
 import { FaCableCar } from "react-icons/fa6";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+    const [cart] = useCart();
     return (
         <div className="flex">
             {/* dashboard side bar */}
-            <div className="w-64 min-h-screen bg-orange-400">
+            <div className="w-64 min-h-screen bg-orange-400 text-black">
                 <ul className="menu">
                     <li><NavLink to="/dashboard/userHome">
                         <FaHome></FaHome>
@@ -14,11 +16,11 @@ const Dashboard = () => {
                     </li>
                     <li><NavLink to="/dashboard/reservation">
                         <FaCableCar></FaCableCar>
-                        User Home</NavLink>
+                        Reservation</NavLink>
                     </li>
                     <li><NavLink to="/dashboard/cart">
                         <FaShoppingCart></FaShoppingCart>
-                        My Cart</NavLink>
+                        My Cart ({cart.length})</NavLink>
                     </li>
                     <li><NavLink to="/dashboard/review">
                         <FaAd></FaAd>
@@ -28,10 +30,20 @@ const Dashboard = () => {
                         <FaList></FaList>
                         My Bookings</NavLink>
                     </li>
+                    {/* Divider */}
+                    <div className="divider"></div>
+                    <li><NavLink to="/">
+                        <FaHome></FaHome>
+                        Home</NavLink>
+                    </li>
+                    <li><NavLink to="/order/salad">
+                        <FaSearch></FaSearch>
+                        Menu</NavLink>
+                    </li>
                 </ul>
             </div>
             {/* dashboard content */}
-            <div className="flex-1">
+            <div className="flex-1 p-8">
                 <Outlet></Outlet>
             </div>
         </div>
